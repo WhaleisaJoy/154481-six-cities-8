2c89e0ec460bef62085f4ce8cd40754417ea3fc6
Maria <afterlifelineonomega@gmail.com>
2021-10-11 21:57:38 +0300

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,40 @@
-import Main from '../main/main';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from '../pages/main/main';
+import Login from '../pages/login/login';
+import Favorites from '../pages/favorites/favorites';
+import Property from '../pages/property/property';
+import NotFoundPage from '../pages/not-found-page/not-found-page';
+import { AppRoute } from '../../const';
 
 type AppProps = {
   placeCardCount: number;
 }
 
 function App({placeCardCount}: AppProps): JSX.Element {
+  // return (
+  //   <Main placeCardCount={placeCardCount} />
+  // );
+
   return (
-    <Main placeCardCount={placeCardCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <Main placeCardCount={placeCardCount} />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.Room}>
+          <Property />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
