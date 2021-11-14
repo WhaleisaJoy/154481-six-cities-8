b6e56d58f359016212a921592fa072375cbca321
Maria <afterlifelineonomega@gmail.com>
2021-11-14 23:28:31 +0300

    Рефакторинг

--- project/src/components/host/host.tsx
+++ project/src/components/host/host.tsx
@@ -7,11 +7,15 @@ type HostType = {
 function Host({offer}: HostType): JSX.Element {
   const { host, description } = offer;
 
+  const proUserClassName = host.isPro
+    ? 'property__avatar-wrapper--pro'
+    : '';
+
   return (
     <div className="property__host" id={host.id.toString()}>
       <h2 className="property__host-title">Meet the host</h2>
       <div className="property__host-user user">
-        <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
+        <div className={`property__avatar-wrapper ${proUserClassName} user__avatar-wrapper`}>
           <img className="property__avatar user__avatar" src={host.avatarUrl} width="74" height="74" alt="Host avatar" />
         </div>
         <span className="property__user-name">
