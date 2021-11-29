8a84e7cbb40f79b95b1ee33357f342efb253029a
Maria <afterlifelineonomega@gmail.com>
2021-11-29 08:28:18 +0300

    Исправляет ошибки

--- project/src/components/cities-list/cities-list.test.tsx
+++ project/src/components/cities-list/cities-list.test.tsx
@@ -5,12 +5,13 @@ import { Router } from 'react-router-dom';
 import { createAPI } from '../../services/api';
 import thunk, {ThunkDispatch} from 'redux-thunk';
 import { configureMockStore } from '@jedmao/redux-mock-store';
+// import userEvent from '@testing-library/user-event';
 import { StateType } from '../../types/state';
 import { Action } from 'redux';
 import { Cities } from '../../database';
 import CitiesList from './cities-list';
 
-const DEFAULT_CITY = Cities.PARIS;
+const CURRENT_CITY = Cities.PARIS;
 
 const onFakeUnauthorized = jest.fn();
 const api = createAPI(onFakeUnauthorized());
@@ -20,7 +21,11 @@ const mockStore = configureMockStore<
     Action,
     ThunkDispatch<StateType, typeof api, Action>
   >(middlewares);
-const store = mockStore();
+const store = mockStore({
+  INTERFACE: {
+    city: CURRENT_CITY,
+  },
+});
 const history = createMemoryHistory();
 
 describe('Component: CitiesList', () => {
@@ -28,12 +33,31 @@ describe('Component: CitiesList', () => {
     const { container } = render(
       <Provider store={store}>
         <Router history={history}>
-          <CitiesList city={DEFAULT_CITY} />
+          <CitiesList city={CURRENT_CITY} />
         </Router>
       </Provider>,
     );
 
-    expect(screen.getByText(DEFAULT_CITY).parentElement).toHaveClass('tabs__item--active');
+    expect(screen.getByText(CURRENT_CITY).parentElement).toHaveClass('tabs__item--active');
     expect(container.querySelectorAll('.locations__item').length).toEqual(Object.values(Cities).length);
   });
+
+  // it('should dispatch changeCity when user click tab', () => {
+  //   const onChooseCity = jest.fn();
+  //   const notCurrentCity = Cities.AMSTERDAM;
+  //   const { container } = render(
+  //     <Provider store={store}>
+  //       <Router history={history}>
+  //         <CitiesList city={CURRENT_CITY} />
+  //       </Router>
+  //     </Provider>,
+  //   );
+
+  //   const notCurrentCityLink = screen.getByRole('link');
+
+  //   userEvent.click(notCurrentCityLink);
+
+  //   expect(screen.getByText(CURRENT_CITY).parentElement).toHaveClass('tabs__item--active');
+  //   expect(container.querySelectorAll('.locations__item').length).toEqual(Object.values(Cities).length);
+  // });
 });
